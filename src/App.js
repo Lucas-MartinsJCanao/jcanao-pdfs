e3df439b8d5fb381728bc6b0896f9835b0488eda
Lucas Martins <Lucas.Martins@jcanao.pt>
2023-02-13 16:26:27 +0000

    feat add : navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React from 'react';
+import Navbar from './components/AppBar';
+
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Navbar/>
+    <div>
+      <div 
+    style = {{
+      margin: 'auto',
+      width: '50%',
+      marginTop: '10%',
+      borderRadius: '10px',
+      boxShadow: '0 0 10px 0 rgba(0,0,0.2,0.2)',
+      padding: '20px',
+      backgroundColor: 'white',
+      
+    }}>
 
     </div>
+    <div>
+      
+    </div>
+    </div>
+    
+    </>
+    
   );
 }
 
