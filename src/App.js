46ab8bcdb4a8ea889646fc2d62fd2400858c8c36
Lucas Martins <Lucas.Martins@jcanao.pt>
2023-02-14 11:20:57 +0000

    feat add : new installs

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
 import React from 'react';
 import Navbar from './components/AppBar';
 import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
 import SendIcon from '@mui/icons-material/Send';
 import Stack from '@mui/material/Stack';
 
+
 function App() {
+
   return (
     <>
-    <Navbar/>
-    <div>
-      <div 
-    style = {{
-      margin: 'auto',
-      width: '50%',
-      marginTop: '10%',
-      borderRadius: '10px',
-      boxShadow: '0 0 10px 0 rgba(0,0,0.2,0.2)',
-      padding: '20px',
-      backgroundColor: 'white',
-      
-    }}>
-    </div>
-    <div 
-    style = {{
-      margin: 'auto',
-      width: '50%',
-      marginTop: '2%',
-    }}>
-       <Stack direction="row" spacing={2}>
-      <Button variant="contained" endIcon={<SendIcon />}>
-        Send
-      </Button>
-    </Stack>
-    </div>
-    </div>
-    
+      <Navbar />
+      <div>
+        <div className="pdfContainer" style={{
+          margin: 'auto',
+          width: '50%',
+          marginTop: '10%',
+          borderRadius: '10px',
+          boxShadow: '0 0 10px 0 rgba(0,0,0.2,0.2)',
+          padding: '20px',
+          backgroundColor: 'white',
+        }}>
+        
+        </div>
+        <div style={{
+          margin: 'auto',
+          width: '50%',
+          marginTop: '2%',
+        }}>
+          <Stack direction="row" spacing={2}>
+            <Button variant="contained" endIcon={<SendIcon />}>
+              Send
+            </Button>
+          </Stack>
+        </div>
+      </div>
     </>
-    
   );
 }
 
